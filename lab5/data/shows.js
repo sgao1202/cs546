cf32ec2fbcfbe744c4bb0b7bb1c298ce4b713e56
Simon Gao <sgao1202@gmail.com>
2020-10-09 13:43:57 -0400

    final submission

--- lab5/data/shows.js
+++ lab5/data/shows.js
@@ -1,14 +1,22 @@
 const axios = require('axios');
+let shows = undefined;
 
 async function getShows() {
-    const { data } = await axios.get('http://api.tvmaze.com/shows');
-    return data;
+    if (!shows) {
+        const { data } = await axios.get('http://api.tvmaze.com/shows');
+        shows = data;
+    }
+    return shows;
 }
 
 async function getShowById(id) {
-    if (isNaN(id) || parseInt(id) <= 0) throw 'Id must be a positive whole number';
-    const { data } = await axios.get(`http://api.tvmaze.com/shows/${id}`);
-    if (data.status === 404) throw `Show with id=${id} cannot be found`;
+    // Throw if the id argument does not exist, is not a number, is a float, or if the number <= 0
+    if (!id || isNaN(id) || id.includes('.') || parseInt(id) <= 0) throw 'Id must be a positive whole number';
+
+    const { data } = await axios.get(`http://api.tvmaze.com/shows/${id}`).catch((error) => {
+        // Custom error handling rather than returning the JSON object from axios.get
+        throw `Show with id=${id} cannot be found`;
+    });
     return data;
 }
 
